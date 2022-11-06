928ed8846648a44868b0e7771ef0626790abb883
NiceTulipe <lyubovchenko@yandex.ru>
2022-11-06 12:04:46 +0300

    аааа

--- src/Ai95Calculator.java
+++ src/Ai95Calculator.java
@@ -2,7 +2,8 @@ class Ai95Calculator {
     double price = 46.35;
 
     public double calculate(double volume, double discount) {
-        double fullPrice = price * (1 + discount);
+        double fullPrice = price * (1 - discount);
+//        System.out.println("fullPrice = " + fullPrice);
         return volume * fullPrice;
     }
 }
--- src/PetrolCalculatorDebug.java
+++ src/PetrolCalculatorDebug.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class PetrolCalculatorDebug {
     public static void main(String[] args) {
         new PetrolCalculatorDebug().calculate();
+
     }
 
     public void calculate() {
